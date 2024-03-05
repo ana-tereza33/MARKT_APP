42700e72d3c2bf6248803b678d92a75a97da33fb
Charlotte Enzweiler <enzweiler.crj@gmail.com>
2024-03-05 17:49:26 +0100

    seedsfixing

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -4,5 +4,5 @@ class Farmer < ApplicationRecord
   has_many_attached :sub_photos
   validates :latitude, presence: true
   validates :longitude, presence: true
-  validates :description, presence: true
+  validates :description_farm, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :adress, presence: true
-        end 
+  validates :address, presence: true
+end
