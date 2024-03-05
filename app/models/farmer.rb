769e569e993dae5d977d4c5b9b6dcc0bac2448ad
Charlotte Enzweiler <156197207+Charly-Enzweiler@users.noreply.github.com>
2024-03-05 14:38:12 +0100

    Update farmer.rb

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,5 +1,7 @@
 class Farmer < ApplicationRecord
   belongs_to :user
+  has_one_attached :main_photo
+  has_many_attached :sub_photos
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates :description, presence: true
