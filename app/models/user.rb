9642563afb1abcc4ee7a5869d30bedaa2fbbf5bd
Carsten Brodersen <carsten.brodersen@web.de>
2024-03-06 12:21:19 +0100

    seeds fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :address, presence: true
+  # validates :address, presence: true
+  has_one :farmer, dependent: :destroy
 end
