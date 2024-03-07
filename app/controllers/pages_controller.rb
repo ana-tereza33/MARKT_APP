0e730bb89e37321b112207d97c5de168cdcb88fc
Charlotte Enzweiler <enzweiler.crj@gmail.com>
2024-03-07 14:56:39 +0100

    corrections last post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if current_user.is_farmer && current_user.farmer.nil?
-      redirect_to new_farmer_path
-    end
+    redirect_to new_farmer_path if user_signed_in? && current_user.is_farmer? && current_user.farmer.nil?
   end
 end
