ed08aebf9bf932e52599ab4a28a4cd9f54b95254
AnaTereza VictorBarros <a.tereza783@gmail.com>
2024-03-15 13:53:26 +0100

    chat fixed

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,13 +6,20 @@ class ChatroomsController < ApplicationController
 
   def create
     chatroom = Chatroom.find_by(user_id: current_user.id, farmer_id: params[:chatroom][:farmer_id])
+
     unless chatroom.nil?
+      farmer_on_page = Farmer.find(params[:chatroom][:farmer_id])
+      chatroom = Chatroom.find_by(user_id: farmer_on_page.user.id, farmer_id: current_user.farmer.id)
+    end
+    
+    unless chatroom.nil?
+      redirect_to chatroom_path(chatroom)
+    else
+
+      @chatroom = Chatroom.new(chatroom_params)
+      @chatroom.save!
       redirect_to chatroom_path(@chatroom)
     end
-
-    @chatroom = Chatroom.new(chatroom_params)
-    @chatroom.save!
-    redirect_to chatroom_path(@chatroom)
   end
 
   private
